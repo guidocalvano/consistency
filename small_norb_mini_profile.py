eec20fd9767eace4fe26ca0cae2b6d05888dacb8
Guido Sales Calvano <guidocalvano@yahoo.com>
2018-06-25 00:57:16 +0200

    mini profiling working

---
+++ small_norb_mini_profile.py
@@ -0,0 +1,65 @@
+from input.SmallNorb import SmallNorb
+from learning.MatrixCapsNetEstimator import MatrixCapsNetEstimator
+import dill as pickle
+import config
+import numpy as np
+import tensorflow as tf
+
+
+def small_norb_mini_profile():
+    sn = SmallNorb.from_cache()
+
+    print("loaded data")
+
+    mcne = MatrixCapsNetEstimator().init()
+
+    batch_size = 1
+    epoch_count = 1
+    max_steps = 1
+
+    def create_small_input_fn(fn):
+        def input_fn():
+            set = fn()
+            return tf.data.Dataset.from_tensor_slices((tf.gather(set[0], [0]), tf.gather(set[1], [0]))).batch(batch_size)
+        return input_fn
+
+    estimator = mcne.create_estimator(sn, config.TF_DEBUG_MODEL_PATH, epoch_count)
+    train_fn = create_small_input_fn(sn.default_training_set)
+    validation_fn = create_small_input_fn(sn.default_validation_set)
+    test_fn = create_small_input_fn(sn.default_test_set)
+
+    print("loaded data")
+
+    training_profiler = tf.train.ProfilerHook(
+        save_steps=1,
+        save_secs=None,
+        output_dir=config.TF_DEBUG_TRAINING_TIMELINE_FILE_PATH,
+        show_dataflow=True,
+        show_memory=True
+    )
+
+    eval_profiler = tf.train.ProfilerHook(
+        save_steps=1,
+        save_secs=None,
+        output_dir=config.TF_DEBUG_EVAL_TIMELINE_FILE_PATH,
+        show_dataflow=True,
+        show_memory=True
+    )
+
+    train_spec = tf.estimator.TrainSpec(
+        input_fn=train_fn,
+        max_steps=max_steps,
+        hooks=[training_profiler]
+    )
+
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn=validation_fn,
+        hooks=[eval_profiler]
+    )
+    print("training estimator")
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+    print("estimator trained")
+
+
+if __name__ == "__main__":
+    small_norb_mini_profile()
