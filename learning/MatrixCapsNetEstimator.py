b7577e48b56f5e71f93a53db5753fc3b29473302
guidocalvano <guidocalvano@gmail.com>
2019-03-26 10:40:54 +0100

    WIP must still modify model_fn

--- learning/MatrixCapsNetEstimator.py
+++ learning/MatrixCapsNetEstimator.py
@@ -75,40 +75,10 @@ class MatrixCapsNetEstimator:
         batch_size = tf.cast(tf.gather(tf.shape(examples), 0), tf.float32)
 
         is_training = tf.constant(mode == tf.estimator.ModeKeys.TRAIN)
-        routing_state = None
-
-        mcn = MatrixCapsNet()
-
-        if self.inialization:
-            mcn.set_init_options(self.inialization)
-
-        if self.regularization:
-            mcn.set_regularization(self.regularization)
-
-        # GET ARCHITECTURE FROM MATRIX CAPSNET
-        network_output, reset_routing_configuration_op, regularization_loss = \
-            getattr(mcn, self.architecture)(examples, iteration_count, routing_state)
-
-        (activations, poses) = network_output
-
-        predicted_classes = tf.reshape(tf.argmax(activations, 1), [-1])
-        if mode == tf.estimator.ModeKeys.PREDICT:
-            predictions = {
-                'class_ids': predicted_classes[:, tf.newaxis],
-                'probabilities': tf.nn.softmax(activations),
-                'activations': activations,
-                'poses': poses
-            }
-            return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
         processed_example_counter = MatrixCapsNetEstimator.counter(batch_size, is_training)
 
-        spread_loss_margin = MatrixCapsNetEstimator.spread_loss_margin(processed_example_counter)
-
-        loss = self.loss_fn(tf.one_hot(labels, label_count), tf.reshape(activations, [-1, label_count]), {
-            "margin": spread_loss_margin})
-
-        if regularization_loss is not None: loss = loss + regularization_loss * self.regularization['axial']
+        #@TODO: parallelize using: https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/cifar10_multi_gpu_train.py#L165
 
         # Compute evaluation metrics.
         accuracy = tf.metrics.accuracy(labels=labels,
@@ -160,6 +130,80 @@ class MatrixCapsNetEstimator:
         )
 
         return train_spec
+
+    def network_loss(self, examples, labels, label_count, iteration_count, processed_example_counter):
+
+        routing_state = None
+
+        mcn = MatrixCapsNet()
+
+        if self.inialization:
+            mcn.set_init_options(self.inialization)
+
+        if self.regularization:
+            mcn.set_regularization(self.regularization)
+
+        # GET ARCHITECTURE FROM MATRIX CAPSNET
+        network_output, reset_routing_configuration_op, regularization_loss = \
+            getattr(mcn, self.architecture)(examples, iteration_count, routing_state)
+
+        (activations, poses) = network_output
+
+        predicted_classes = tf.reshape(tf.argmax(activations, 1), [-1])
+        if mode == tf.estimator.ModeKeys.PREDICT:
+            predictions = {
+                'class_ids': predicted_classes[:, tf.newaxis],
+                'probabilities': tf.nn.softmax(activations),
+                'activations': activations,
+                'poses': poses
+            }
+            return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+        spread_loss_margin = MatrixCapsNetEstimator.spread_loss_margin(processed_example_counter)
+
+        loss = self.loss_fn(tf.one_hot(labels, label_count), tf.reshape(activations, [-1, label_count]), {
+            "margin": spread_loss_margin})
+
+        if regularization_loss is not None: loss = loss + regularization_loss * self.regularization['axial']
+
+        return loss
+
+
+
+    def average_gradients(self, tower_grads):
+        """Calculate the average gradient for each shared variable across all towers.
+        Note that this function provides a synchronization point across all towers.
+        Args:
+          tower_grads: List of lists of (gradient, variable) tuples. The outer list
+            is over individual gradients. The inner list is over the gradient
+            calculation for each tower.
+        Returns:
+           List of pairs of (gradient, variable) where the gradient has been averaged
+           across all towers.
+        """
+        average_grads = []
+        for grad_and_vars in zip(*tower_grads):
+            # Note that each grad_and_vars looks like the following:
+            #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+            grads = []
+            for g, _ in grad_and_vars:
+                # Add 0 dimension to the gradients to represent the tower.
+                expanded_g = tf.expand_dims(g, 0)
+
+                # Append on a 'tower' dimension which we will average over below.
+                grads.append(expanded_g)
+
+            # Average over the 'tower' dimension.
+            grad = tf.concat(axis=0, values=grads)
+            grad = tf.reduce_mean(grad, 0)
+
+            # Keep in mind that the Variables are redundant because they are shared
+            # across towers. So .. we will just return the first tower's pointer to
+            # the Variable.
+            v = grad_and_vars[0][1]
+            grad_and_var = (grad, v)
+            average_grads.append(grad_and_var)
+        return average_grads
 
     def train_and_test(self, small_norb, batch_size=64, epoch_count=600, max_steps=None, save_summary_steps=500, eval_steps=100, model_path=config.TF_MODEL_PATH):
 
